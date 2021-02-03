1a5a6cfe536517d57564a84ac943afdb5687bf30
stefan gulas <stefan.gulas@gmail.com>
2021-02-03 21:15:43 +0100

    Ausgabe Gereinigte Datei, Grundgerüst geschaffen

--- VermessungsBüroApp/MainWindow.xaml.cs
+++ VermessungsBüroApp/MainWindow.xaml.cs
@@ -1,23 +1,14 @@
 ﻿using Microsoft.Win32;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace VermessungsBüroApp
 {
-    
+
     public partial class MainWindow : Window
     {
         private MainViewModel _viewModel;
@@ -28,12 +19,14 @@ namespace VermessungsBüroApp
             set { _viewModel = value; }
         }
 
+        public string[] rawLines { get; private set; }
 
         public MainWindow()
         {
             InitializeComponent();
             _viewModel = new MainViewModel();
             DataContext = _viewModel;
+            string[] rawLines = new string[] { };
 
         }
 
@@ -66,18 +59,20 @@ namespace VermessungsBüroApp
         {
 
         }
-
+        public string FileNameCleanedFile { get; set; }
         private void OpenFileButton_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog(); 
+            OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-            if (openFileDialog.ShowDialog() == true) 
-            { 
+            if (openFileDialog.ShowDialog() == true)
+            {
                 PunkteFenster.Text = File.ReadAllText(openFileDialog.FileName);
-                
+                FileNameCleanedFile = openFileDialog.FileName;
+                rawLines = File.ReadAllLines(openFileDialog.FileName);
+
             }
         }
-        
+
 
         private void SaveFileButton_Click(object sender, RoutedEventArgs e)
         {
@@ -86,18 +81,46 @@ namespace VermessungsBüroApp
 
         private void CleanFileButton_Click(object sender, RoutedEventArgs e)
         {
-            
-                string line;
-                List<string> rawLines = new List<string>();
-                //while ((line = PunkteFenster.Text.ToList()) != null)
-                //{
-                //    rawLines.Add(line);
-                //}
-                //foreach (var file in rawLines)
-                //{
-                //    GesäubertesPunkteFenster.Text = file;
-                //}
-            
+            var zeileReinigen = new ZeileReinigen();
+            List<string> gereinigteListe = new List<string>();
+            foreach (var line in rawLines)
+            {
+                string gereinigteZeile = zeileReinigen.CleaneLine(line);
+                gereinigteListe.Add(gereinigteZeile);
+            }
+            string gereingteDatei = string.Empty;
+            foreach (var item in gereinigteListe)
+            {
+                gereingteDatei += item;
+            }
+            GesäubertesPunkteFenster.Text = gereingteDatei;
+            string newFile1 = @"C:\Users\stefa\OneDrive\Dokumente\Forster\Auswertung1\";
+            string newFile2 = @"C:\Users\stefa\OneDrive\Dokumente\Forster\Auswertung2\";
+            newFile1 += Path.GetFileName(FileNameCleanedFile);
+            newFile2 += Path.GetFileName(FileNameCleanedFile);
+
+            using (StreamWriter writer = new StreamWriter(newFile1))
+            {
+                foreach (var line in gereinigteListe) writer.WriteLine(line);
+            }
+            using (StreamWriter writer = new StreamWriter(newFile2))
+            {
+                foreach (var line in gereinigteListe) writer.WriteLine(line);
+            }
+            //if (line.Count() < 3) continue;
+            //    if (line.Count() == 3)
+            //    {
+            //        line.Insert(1, "205");
+            //        line.Insert(4, "     0");
+            //    }
+            //    if (line.Count() == 4 && line[1].Length == 2)
+            //    {
+            //        cleanedLineItems[1] = " " + cleanedLineItems[1];
+            //        cleanedLineItems.Insert(4, "     0");
+            //    }
+        }
+
+    
         }
     }
-}
+
