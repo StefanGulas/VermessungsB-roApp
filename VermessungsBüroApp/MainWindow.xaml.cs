8b1b7ecb3666c21c6cae7e8287624f16009c50ed
stefan gulas <stefan.gulas@gmail.com>
2021-02-05 20:27:47 +0100

    Refactoring

--- VermessungsBüroApp/MainWindow.xaml.cs
+++ VermessungsBüroApp/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using Microsoft.CodeAnalysis;
-using Microsoft.Win32;
+﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Text.Unicode;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using Path = System.IO.Path;
 
 namespace VermessungsBüroApp
 {
